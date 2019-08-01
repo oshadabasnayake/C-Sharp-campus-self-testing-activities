b30576f2451ff105c9fd7cb0d1c9a8993ca516b7
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-08-01 22:02:10 +0530

    concatanation
    concatanation example

---
+++ concatanation/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your username");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("hello " + username + " welcome & you are " + age + " old.");
+
+            Console.ReadKey();
+        }
+    }
+}
