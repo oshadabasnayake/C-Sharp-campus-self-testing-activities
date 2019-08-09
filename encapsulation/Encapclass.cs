fd1be42de13661af121fed509c54506ddfcb69fd
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-08-09 19:27:23 +0530

    Encapclass
    Encapclass added

---
+++ encapsulation/Encapclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Encapclass
+    {
+        private int age;
+
+        public void setAge(int a)
+        {
+            age = a;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+    }
+}
