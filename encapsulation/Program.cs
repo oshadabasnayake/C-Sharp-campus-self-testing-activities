a37b238fda8424b0aa770aa440050c4f46b64d10
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-08-10 23:29:17 +0530

    Program
    Program file added

---
+++ encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapclass e1 = new Encapclass();
+            e1.setAge(50);
+
+            Console.WriteLine("Age is " + e1.getAge());
+
+            Console.ReadKey();
+        }
+    }
+}
