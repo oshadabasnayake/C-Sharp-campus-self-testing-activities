1348af9b2415bc20a1e021fc067dd1ee0c6f7003
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-08-12 21:09:45 +0530

    for loop
    for loop

---
+++ for loop.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oshe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 10; x < 15; x++)
+            {
+                Console.WriteLine("Value of x: {0}", x);
+            }
+        }
+    }
+}
